b829582637efa7888ddb3f1722868607800a8cf1
GojkovicM <marko.gojkovic@yahoo.com>
2024-02-05 12:22:24 +0100

    newQuickTrade

--- src/Components/QuickTrade/QuickTrade.styled.js
+++ src/Components/QuickTrade/QuickTrade.styled.js
@@ -1,8 +1,64 @@
 import styled from "styled-components";
 
+const theme = {
+  colors: {
+    background: "#202022",
+    primary: "#ff971d", //naranadzassta
+    text: "#f1f3f4",
+    buttonBackground: "rgba(128, 128, 128)",
+  },
+};
 
 export const StyledQuickTrade = styled.div`
-    border: 1px solid black;
-    
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  color: ${theme.colors.text};
+  padding: 1.3rem 1rem 1.3rem 1rem;
+  margin-top: 1.3rem;
+  height: 18rem;
 
-`
+  div {
+    padding: 0.7rem 0 0.7rem 0;
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    ul {
+      background-color: ${theme.colors.buttonBackground};
+      padding: 0.7rem;
+
+      .content {
+        position: absolute;
+      }
+    }
+
+    li {
+      display: none;
+    }
+    ul:hover li {
+      display: block;
+      background-color: ${theme.colors.buttonBackground};
+      z-index: 1;
+    }
+  }
+  .button {
+    display: flex;
+    justify-content: space-between;
+  }
+  button:first-child {
+    background-color: green;
+    color: white;
+    width: 48%;
+    border: none;
+    padding: 0.3rem;
+  }
+  button:nth-child(2) {
+    background-color: red;
+    color: white;
+    width: 48%;
+    border: none;
+  }
+`;
--- src/Components/QuickTrade/QuickTrade.tsx
+++ src/Components/QuickTrade/QuickTrade.tsx
@@ -2,15 +2,42 @@ import React from "react";
 import { StyledQuickTrade } from "./QuickTrade.styled";
 
 function QuickTrade() {
-    return ( 
-        <StyledQuickTrade>
-            QuickTrade <br />
-            Amount <br />
-            Price <br />
-            Total  <br />
-            Buy/Sell <br />
-        </StyledQuickTrade>
-     );
+  return (
+    <StyledQuickTrade>
+      <div className="wrapper">
+        <h3>Quick Trade</h3>
+        <ul>
+          {" "}
+          Izaberi:
+          <img src="" alt="" />
+          <div className="content">
+            <li>marko</li>
+            <li>branko</li>
+            <li>darko</li>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <span>Amount BTC</span> <input type="text" />
+      </div>
+      <div>
+        <span>Amount BTC</span> <input type="text" />
+      </div>
+      <div>
+        <span>Amount BTC</span> <input type="text" />
+      </div>
+      <div className="button">
+        <button>
+          Buy
+          <img src="" alt="" />
+        </button>
+        <button>
+          Sell
+          <img src="" alt="" />
+        </button>
+      </div>
+    </StyledQuickTrade>
+  );
 }
 
 export default QuickTrade;
